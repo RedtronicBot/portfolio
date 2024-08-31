489356082dbc9c19127d9c1d7d45bb4ead304bcd
Redtronic <redtronicmc@gmail.com>
2024-08-31 18:38:10 +0200

    correction index

--- src/index.jsx
+++ src/index.jsx
@@ -15,7 +15,7 @@ import BetWise from './pages/projets/BetWise'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<Router>
+		<Router basename='/'>
 			<Routes>
 				<Route path='/' element={<Accueil/>}/>
 				<Route path='/booki' element={<Booki/>}/>
